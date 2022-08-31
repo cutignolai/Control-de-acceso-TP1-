84a4ece6eaf5b9c16650fc5c7b9508011402c9dc
odevincenti <odevincenti@itba.edu.ar>
2022-08-31 00:56:32 -0300

    Timer listo, no testeado

---
+++ drivers/timer.c
@@ -0,0 +1,191 @@
+/***************************************************************************//**
+  @file     timer.c
+  @brief    Timer driver. Advance implementation
+  @author   Nicolás Magliola y Olivia De Vincenti
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "timer.h"
+
+#include "SysTick.h"
+
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#if TIMER_TICK_MS != (1000U/SYSTICK_ISR_FREQUENCY_HZ)
+#error Las frecuencias no coinciden!!
+#endif // TIMER_TICK_MS != (1000U/SYSTICK_ISR_FREQUENCY_HZ)
+
+#define TIMER_DEVELOPMENT_MODE    1
+
+#define TIMER_ID_INTERNAL   0
+
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef struct {
+	ttick_t             period;
+	ttick_t             cnt;
+    tim_callback_t      callback;
+    uint8_t             mode        : 1;
+    uint8_t             running     : 1;
+    uint8_t             expired     : 1;
+    uint8_t             unused      : 5;
+} timer_t;
+
+
+/*******************************************************************************
+ * VARIABLES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Periodic service
+ */
+static void timer_isr(void);
+
+
+/*******************************************************************************
+ * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static timer_t timers[TIMERS_MAX_CANT];
+static tim_id_t timers_cant = TIMER_ID_INTERNAL+1;
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void timerInit(void)
+{
+    static bool yaInit = false;
+    if (yaInit)
+        return;
+    
+    SysTick_Init(timer_isr); // init peripheral
+    
+    yaInit = true;
+}
+
+
+tim_id_t timerGetId(void)
+{
+#ifdef TIMER_DEVELOPMENT_MODE
+    if (timers_cant >= TIMERS_MAX_CANT)
+    {
+        return TIMER_INVALID_ID;
+    }
+    else
+#endif // TIMER_DEVELOPMENT_MODE
+    {
+        return timers_cant++;
+    }
+}
+
+
+void timerStart(tim_id_t id, ttick_t ticks, uint8_t mode, tim_callback_t callback)
+{
+#ifdef TIMER_DEVELOPMENT_MODE
+    if ((id < timers_cant) && (mode < CANT_TIM_MODES))
+#endif // TIMER_DEVELOPMENT_MODE
+    {
+        timers[id].running = 0;
+        timers[id].expired = 0;
+        timers[id].cnt = 0;
+        timers[id].mode = mode;
+        timers[id].period = ticks;
+        timers[id].callback = callback;
+    }
+}
+
+
+// Apaga el timer pero no lo reinicia
+void timerStop(tim_id_t id){
+    timers[id].running = 0;
+    timers[id].expired = 0;
+}
+
+
+void timerReset(tim_id_t id){
+    timers[id].running = 0;
+    timers[id].cnt = timers[id].period;
+    timers[id].running = 1;
+    timers[id].expired = 0;
+}
+
+// Expira el timer y lo hace llegar a cero
+void timerFinish(tim_id_t id){
+    timers[id].running = 0;
+    timers[id].expired = 1;
+    timers[id].cnt = 0;
+    timers[id].callback();
+}
+
+// Resetea un timer apagado
+void timerReset(tim_id_t id){
+    timers[id].running = 0;
+    timers[id].expired = 0;
+    timers[id].cnt = 0;
+}
+
+// Indica si expiró o no el timer
+bool timerExpired(tim_id_t id){
+    bool is_expired = timers[id].expired;
+    timers[id].expired = 0;
+    return is_expired;
+}
+
+
+void timerDelay(ttick_t ticks)
+{
+    timerStart(TIMER_ID_INTERNAL, ticks, TIM_MODE_SINGLESHOT, NULL);
+    while (!timerExpired(TIMER_ID_INTERNAL))
+    {
+        // wait...
+    }
+}
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+// Recorre el arreglo de timers y busca si expiró alguno
+static void timer_isr(void){
+    tim_id_t id_idx;
+    for (id_idx = timers_cant; id_idx; id_idx--){
+        if (timers[id_idx].running && timers[id_idx].cnt){
+            timers[id_idx].cnt--;
+            if (!timers[id_idx].cnt){
+                if (timers[id_idx].mode == TIM_MODE_SINGLESHOT){
+                    timers[id_idx].expired = 1;
+                    timers[id_idx].running = 0;
+                }
+                if (timers[id_idx].callback != NULL){
+                    timers[id_idx].callback();
+                }
+            }
+        }
+    }
+}
+
+/******************************************************************************/
--- drivers/timer.h
+++ drivers/timer.h
@@ -1,28 +1,44 @@
 /***************************************************************************//**
   @file     timer.h
-  @brief    Timer driver
-  @author   Olivia De Vincenti
+  @brief    Timer driver. Advance implementation
+  @author   Nicolás Magliola y Olivia De Vincenti
  ******************************************************************************/
 
-#ifndef _SYSTICK_H_
-#define _SYSTICK_H_
+#ifndef _TIMER_H_
+#define _TIMER_H_
 
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
 
 #include <stdint.h>
-#include "SysTick.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
+#define TIMER_TICK_MS       1
+#define TIMER_MS2TICKS(ms)  ((ms)/TIMER_TICK_MS)
+
+#define TIMERS_MAX_CANT     16
+#define TIMER_INVALID_ID    255
+
+
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
-typedef uint8_t Timer_ID;
+// Timer Modes
+enum { TIM_MODE_SINGLESHOT, TIM_MODE_PERIODIC, CANT_TIM_MODES };
+
+// Timer alias
+typedef uint32_t ttick_t;
+typedef uint8_t tim_id_t;
+typedef void (*tim_callback_t)(void);
+
 
 /*******************************************************************************
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
@@ -33,17 +49,71 @@ typedef uint8_t Timer_ID;
  ******************************************************************************/
 
 /**
- * @brief Initialize SysTic driver
- * @param funcallback Function to be call every SysTick
- * @return Initialization and registration succeed
+ * @brief Initialice timer and corresponding peripheral
  */
-bool Timer_Init (void);
+void timerInit(void);
 
+
+// Non-Blocking services ////////////////////////////////////////////////
+
+/**
+ * @brief Request a timer
+ * @return ID of the timer to use
+ */
+tim_id_t timerGetId(void);
+
+
+/**
+ * @brief Begin to run a new timer
+ * @param id ID of the timer to start
+ * @param ticks time until timer expires, in ticks
+ * @param mode SINGLESHOT or PERIODIC
+ * @param callback Function to be call when timer expires
+ */
+void timerStart(tim_id_t id, ttick_t ticks, uint8_t mode, tim_callback_t callback);
+
+/**
+ * @brief Stop the count of a timer
+ * @param id ID of the timer to stop
+ */
+void timerStop(tim_id_t id);
+
+/**
+ * @brief Restart timer
+ * @param id ID of the timer to restart
+ */
+void timerRestart(tim_id_t id);
+
+/**
+ * @brief Finish to run a timer
+ * @param id ID of the timer to end
+ */
+void timerFinish(tim_id_t id);
+
+/**
+ * @brief Reset a timer to before it was running, deactivated
+ * @param id ID of the timer to reset
+ */
+void timerReset(tim_id_t id);
+
+/**
+ * @brief Verify if a timer has run timeout
+ * @param id ID of the timer to check for expiration
+ * @return 1 = timer expired
+ */
+bool timerExpired(tim_id_t id);
+
+
+// Blocking services ////////////////////////////////////////////////
+
+/**
+ * @brief Wait the specified time. Use internal timer
+ * @param ticks time to wait in ticks
+ */
+void timerDelay(ttick_t ticks);
 
 
 /*******************************************************************************
  ******************************************************************************/
-
-
 
 #endif // _TIMER_H_
