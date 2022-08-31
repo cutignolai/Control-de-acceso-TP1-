f06d3b5226c6d464a59508ca4de699fc7f3b9fb3
cutignolai <58371858+cutignolai@users.noreply.github.com>
2022-08-31 04:03:52 -0300

    Actualizacion

--- drivers/leds.c
+++ drivers/leds.c
@@ -1,6 +1,6 @@
 /***************************************************************************//**
   @file     leds.c
-  @brief    Leds driver
+  @brief    leds driver
   @author   Micho
  ******************************************************************************/
 
@@ -9,17 +9,8 @@
  ******************************************************************************/
 
 #include "leds.h"
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-#include "timer.h"
-
-=======
-#include "board.h"
->>>>>>> 8f353db74644faa259b72305d4753deabc6f16af
-=======
 //#include "timers_drv.h"
 
->>>>>>> Stashed changes
 /*******************************************************************************
  *            CONSTANT AND MACRO DEFINITIONS USING #DEFINE                    *
  ******************************************************************************/
@@ -30,58 +21,16 @@
 /*******************************************************************************
  *                  ENUMERATIONS AND STRUCTURES AND TYPEDEFS                    *
  ******************************************************************************/
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-typedef struct led_selector_t{
-  bool led_bit_0;
-	bool led_bit_1;
-}led_selector_t;
-=======
-
-//----------- COLORES DEL LED ------------
-typedef enum{
-    LED_ROJO,
-    LED_VERDE,
-    LED_AZUL
-}colored_led_t;
-//-----------------------------------------
->>>>>>> 8f353db74644faa259b72305d4753deabc6f16af
-=======
 typedef struct led_selector_t
 {
 	bool led_bit_0;
 	bool led_bit_1;
 }led_selector_t;
->>>>>>> Stashed changes
 
 /*******************************************************************************
- *      FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE         *
- ******************************************************************************/
-
-static void callback_leds();
-
-/*******************************************************************************
- *                                  VARIABLES                                   *
+ *      STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE            *
  ******************************************************************************/
 static bool leds[NUM_LEDS];
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-static led_selector_t led_selector[] = {
-  {LOW,LOW},    //00 --> NINGUN LED
-  {LOW,HIGH},   //01 --> LED0
-  {HIGH,LOW},   //10 --> LED1
-  {HIGH,HIGH}   //11 --> LED
-};
-static int index = 0;
-static tim_id_t leds_timer;            
-=======
-
-  //static int refresh_rate = 2;        //tasa de refresco del timer
-  //static tim_id_t rate_id;            //VERRRRRRRRRRR
-
-static void callback_leds();
->>>>>>> 8f353db74644faa259b72305d4753deabc6f16af
-=======
 static led_selector_t led_selector[] = {
   {LOW,LOW},    //00 --> NINGUN LED
   {LOW,HIGH},   //01 --> LED1
@@ -93,35 +42,25 @@ static led_selector_t led_selector[] = {
   //static tim_id_t rate_id;            //VERRRRRRRRRRR
 
   static void callback_leds();
->>>>>>> Stashed changes
 
 /*******************************************************************************
  *******************************************************************************
                         GLOBAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
-void initLeds()
+void initLEDs()
 {
   gpioMode(LED_CONF_1, OUTPUT);
 	gpioMode(LED_CONF_2, OUTPUT);
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-  for(int i = 0; i < NUM_LEDS; i++)
-=======
-
-  for(int i=0; i<NUM_LEDS; i++)
->>>>>>> 8f353db74644faa259b72305d4753deabc6f16af
-=======
   for(int i=0; i<DISP_LEDS_NUM;i++)
->>>>>>> Stashed changes
 	{
 		clear_led(i);
 	}
 
-  leds_timer = timerGetId();
-  //Periodic Interuption ---> leds_callback (4ms)
-	timerStart(leds_timer, TIMER_MS2TICKS(4), TIM_MODE_PERIODIC, &callback_leds);
+  //rate_id = timerGetId();
+	//timerStart(rate_id, TIMER_MS2TICKS(my_refresh_rate), TIM_MODE_PERIODIC, &callback_leds);
+
 }
 
 
