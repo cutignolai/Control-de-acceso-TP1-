60df3c18e62b08e9f9712383a3e413a975ee8af4
odevincenti <odevincenti@itba.edu.ar>
2022-09-01 18:06:36 -0300

    Timers funcionan correctamente

--- source/App.c
+++ source/App.c
@@ -16,39 +16,26 @@
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
-#define MAX_UNIT_ID         8
-#define MAX_UNIT_PASS       4
 
-#define READY               true
-#define NOT_READY           !READY
-
-#define USUARIO_VALIDO      true
-#define USUARIO_INVALIDO    !USUARIO_VALIDO
-
-#define ENTER_CLICK         1
-#define BRILLO_CLICK        2
-#define SUPR_CLICK          3
- 
-#define SI                  true
-#define NO                  false
-
-#define ACTIVADO            true
-#define DESACTIVADO         false
 
 /*******************************************************************************
  * ENUMS AND STRUCTURES
  ******************************************************************************/
 
+
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
-void timer_callback();
+void red_callback();
+void blue_callback();
 
 /*******************************************************************************
  * VARIABLES
  ******************************************************************************/
 uint8_t sec = 0;
-tim_id_t sec_timer;
+tim_id_t red_timer;
+tim_id_t blue_timer;
+uint8_t red_state;
 
 /*******************************************************************************
  *******************************************************************************
@@ -59,23 +46,28 @@ tim_id_t sec_timer;
 /* Función que se llama 1 vez, al comienzo del programa */
 void App_Init (void)
 {
-    timerInit(); 
-    sec_timer = timerGetId();
-    timerStart(sec_timer, TIMER_MS2TICKS(1000), TIM_MODE_PERIODIC, timer_callback);
+	sec = 0;
+	timerInit();
+	gpioMode(PIN_LED_RED, OUTPUT);
+	gpioMode(PIN_LED_BLUE, OUTPUT);
+	red_timer = timerGetId();
+	blue_timer = timerGetId();
+	red_state = LED_ACTIVE;
+	gpioWrite(PIN_LED_RED, red_state);
+	gpioWrite(PIN_LED_BLUE, !LED_ACTIVE);
+	timerStart(red_timer, TIMER_MS2TICKS(10000), TIM_MODE_PERIODIC, red_callback);
+	timerStart(blue_timer, TIMER_MS2TICKS(5000), TIM_MODE_SINGLESHOT, blue_callback);
 }
 
 /* Función que se llama constantemente en un ciclo infinito */
 void App_Run (void)
 {
-
-    if (sec == 10){
-        timerStop(sec_timer);
-        printf("Termino");
-    }
-	//nada
+	if (sec == 10){
+		timerStop(red_timer);
+		gpioWrite(PIN_LED_RED, !LED_ACTIVE);
+	}
 
 }
-
 
 /*******************************************************************************
  *******************************************************************************
@@ -83,9 +75,15 @@ void App_Run (void)
  *******************************************************************************
  ******************************************************************************/
 
-void timer_callback(){
+void red_callback(){
     sec++;
-    printf("sec: %i\n", sec/1000);
+    red_state = !red_state;
+    gpioWrite(PIN_LED_RED, red_state);
+    //printf("sec: %i\n", sec/1000);
+}
+
+void blue_callback(){
+	gpioWrite(PIN_LED_BLUE, LED_ACTIVE);
 }
 
 /*******************************************************************************
