ffe321bc08d7ff1a5b4b611cbdb246ed9f5c8385
odevincenti <odevincenti@itba.edu.ar>
2022-09-01 06:54:28 -0300

    Funcionalidades básicas del display

--- drivers/display.c
+++ drivers/display.c
@@ -10,6 +10,7 @@
 #include "gpio.h"
 #include "./source/board.h"
 #include "display.h"
+#include "timer.h"
 
 /*******************************************************************************
  *          CONSTANT AND MACRO DEFINITIONS USING #DEFINE                        *
@@ -21,9 +22,11 @@
 #define OFF     0
 #define SEG_ON  1
 #define SEG_OFF 0
-#define MAX_BRIGHTNESS  10
-#define BLINK_FREQ      500
-#define BUFFER_LEN      8
+#define MAX_BRIGHTNESS  4
+#define BLINK_T         500
+#define SCROLL_T        500
+#define BUFFER_MAX_LEN  16
+#define MAX_REFRESH_T   30
 
 /******* PINS *******/
 #define SEGA    DIO_1
@@ -38,24 +41,16 @@
 #define SEL0    DIO_2
 #define SEL1    DIO_4
 
-/*** DIGIT MASKS ***/
-#define DIG0    0b00
-#define DIG1    0b01
-#define DIG2    0b10
-#define DIG3    0b11
+#define N_PINS  10
+
+/**** DIGITS ****/
+#define DIG0    0
+#define DIG1    1
+#define DIG2    2
+#define DIG3    3
 
 #define GET_SEL0(x) (x & 1)
 #define GET_SEL1(x) ((x >> 1) & 1)
-
-/********** SEGMENT MASKS **********/
-#define MASK_SEGA   0x01 //0b00000001
-#define MASK_SEGB   0x02 //0b00000010
-#define MASK_SEGC   0x04 //0b00000100
-#define MASK_SEGD   0x08 //0b00001000
-#define MASK_SEGE   0x10 //0b00010000
-#define MASK_SEGF   0x20 //0b00100000
-#define MASK_SEGG   0x40 //0b01000000
-#define MASK_SEGDP  0x80 //0b10000000
 
 /************ CHAR MASKS ************/
         //     aaaa
@@ -84,36 +79,30 @@
 /*******************************************************************************
  *               ENUMERATIONS AND STRUCTURES AND TYPEDEFS                       *
  ******************************************************************************/
-// typedef struct {
-// 	digit_t     segA    : 1;
-//     digit_t     segB    : 1;
-//     digit_t     segC    : 1;
-//     digit_t     segD    : 1;
-//     digit_t     segE    : 1;
-//     digit_t     segF    : 1;
-//     digit_t     segG    : 1;
-//     digit_t     segDP   : 1;
-// } segments_t;
+typedef enum {
+    DISPLAY_CLEAR = 0,
+    DISPLAY_SCROLL = 1,
+    DISPLAY_BLINK = 2,
+    DISPLAY_STATIC = 3
+} display_states_t;
 
 /*******************************************************************************
  *                  VARIABLE PROTOTYPES WITH GLOBAL SCOPE                       *
  ******************************************************************************/
 
-digit_t char_arr[] = {NUM_0, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, NUM_7, NUM_8, NUM_9, DASH, LOW_DASH, POINT, CLEAR};
-uint8_t seg_arr[] = {SEGA, SEGB, SEGC, SEGD, SEGE, SEGF, SEGG, SEGDP};
-uint8_t digit_arr[] = {DIG0, DIG1, DIG2, DIG3};
-digit_t display[] = {0, 0, 0, 0};
-digit_t buffer[16];
-uint8_t buffer_len;
-
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
+void refresh_display();
 void set_segment(uint8_t seg, uint8_t set);
 void set_display(digit_t arr[]);
 void set_digit(digit_t number, uint8_t position);
 void set_buffer(digit_t *dig_arr, uint8_t arr_len);
 void clear_buffer();
+void blink_digits();
+void scroll_buffer();
+void display_blink(digit_t* arr);
+void clear_display();
 
 /*******************************************************************************
  * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
@@ -122,6 +111,29 @@ void clear_buffer();
 /*******************************************************************************
  * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
+static digit_t char_arr[] = {NUM_0, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, NUM_7, NUM_8, NUM_9, DASH, LOW_DASH, POINT, CLEAR};
+static uint8_t seg_arr[] = {SEGA, SEGB, SEGC, SEGD, SEGE, SEGF, SEGG, SEGDP};
+
+static display_states_t display_state = CLEAR;
+
+static digit_t buffer[BUFFER_MAX_LEN];
+static uint8_t buffer_len;         // Cantidad de caracteres seteados del buffer
+static uint8_t buffer_idx;         // Caracter a partir del cual mostrar
+
+static uint8_t brightness = 1;
+
+static uint8_t scroll_idx;
+
+static bool show_last = false;
+
+static bool blink = false;
+static bool blinking_digits[DISPLAY_LEN] = { false, false, false, false};
+
+// TIMERS
+static tim_id_t refresh_timer;
+static tim_id_t blink_timer;
+static tim_id_t scroll_timer;
+
 
 
 /*******************************************************************************
@@ -131,26 +143,137 @@ void clear_buffer();
  ******************************************************************************/
 // INIT
 void initDisplay(){
+    
+    // INIT PINS
+    pin_t pin_arr[] = {DIO_1, DIO_3, DIO_5, DIO_7, DIO_9, DIO_11, DIO_13, DIO_15, DIO_2, DIO_4};
+    uint8_t i;
+    for(i = 0; i < N_PINS; i++){
+        gpioMode(pin_arr[i], OUTPUT);
+    }
 
+    // CREATE TIMERS
+    refresh_timer = timerGetId();
+    timerCreate(refresh_timer, TIMER_MS2TICKS(MAX_REFRESH_T/brightness), TIM_MODE_PERIODIC, refresh_display);
+    blink_timer = timerGetId();
+    timerCreate(refresh_timer, TIMER_MS2TICKS(BLINK_T), TIM_MODE_PERIODIC, blink_digits);
+    scroll_timer = timerGetId();
+    timerCreate(refresh_timer, TIMER_MS2TICKS(BLINK_T), TIM_MODE_PERIODIC, scroll_buffer);
+
+    // INIT 7 SEGMENTS
+    clear_buffer();
+
+    // START DISPLAY
+    timerActivate(refresh_timer);
 }                     
 
-// SETTERS
-void clearDisplay(){
+// void showMessage(uint8_t *message, uint8_t message_len){
+//     if (display_state != DISPLAY_MESSAGE){
+//         timerReset(blink_timer);
+//         timerReset(scroll_timer);
+//         if (buffer_len > DISPLAY_LEN){
+//             timerActivate(scroll_timer);
+//         }
+//     }
+//     if (message_len > BUFFER_MAX_LEN) { message_len = BUFFER_MAX_LEN; }
+//     set_buffer(message, message_len);
+//     scroll_idx = 0;
+//     display_state = DISPLAY_MESSAGE;
+// }
 
-    digit_t arr[] = {CLEAR, CLEAR, CLEAR, CLEAR};
-    set_display(&arr);
+void setClearMode(){
+    if (display_state != DISPLAY_CLEAR){
+        timerReset(blink_timer);
+        timerReset(scroll_timer);
+    }
+    display_state = DISPLAY_CLEAR;
 }
 
-void showMessage(uint8_t *message, uint8_t message_len){
-    set_buffer(message, message_len);
+void setScrollMode(){
+    if (display_state != DISPLAY_SCROLL){
+        timerReset(blink_timer);
+        timerRestart(scroll_timer);
+        scroll_idx = 0;
+    }
+    display_state = DISPLAY_SCROLL;
 }
 
-void showPasswordDigit(uint8_t char_idx, uint8_t position){
-    digit_t arr[] = {LOW_DASH, LOW_DASH, LOW_DASH, LOW_DASH};
-    if( position == 4){ position = 3; }
-    arr[position] = char_arr[char_idx];
-    set_buffer(&arr[0], DISPLAY_LEN);
+void setStaticMode(){
+    if (display_state != DISPLAY_STATIC){
+        timerReset(blink_timer);
+        timerReset(scroll_timer);
+    }
+    display_state = DISPLAY_STATIC;
 }
+
+void setBlinkMode(){
+    if (display_state != DISPLAY_BLINK){
+        timerReset(blink_timer);
+        timerReset(scroll_timer);
+        blink = false;
+        timerActivate(blink_timer);
+    }
+    display_state = DISPLAY_BLINK;
+}
+
+void loadBuffer(digit_t *msg, uint8_t msg_len){
+    if (msg_len > BUFFER_MAX_LEN) { msg_len = BUFFER_MAX_LEN; }
+    set_buffer(msg, msg_len);
+    if (show_last) { auto_set_buffer_index(); }
+}
+
+void showLastDigits(bool b){
+    show_last = b;
+    if (show_last) { autoSetBufferIndex(); }
+}
+
+void setBufferIndex(uint8_t idx){
+    if (idx < BUFFER_MAX_LEN - DISPLAY_LEN){
+        buffer_idx = idx;
+    }
+}
+
+void setBlinkingDigits(bool* arr){
+    uint8_t i;
+    for (i = 0; i < DISPLAY_LEN; i++){
+        blinking_digits[i] = *(arr + i);
+    }
+}
+
+
+// void showLastBlinking(uint8_t input_idx){
+//     if (display_state != DISPLAY_BLINK){
+//         timerReset(blink_timer);
+//         timerReset(scroll_timer);
+//         if (buffer_len != BUFFER_MAX_LEN){ 
+//             buffer[buffer_len] = char_arr[input_idx];
+//             buffer_len++; 
+//         } else {
+//             buffer[buffer_len - 1] = char_arr[input_idx];
+//         }
+//         blink = false;
+//         timerActivate(blink_timer);
+//     }
+//     else {
+//         buffer[buffer_len - 1] = char_arr[input_idx];
+//     }
+//     display_state = DISPLAY_BLINK;
+// }
+
+// NO VA EN CAPA DRIVERS
+// void showPasswordDigit(uint8_t char_idx, uint8_t position){
+//     if (display_state != DISPLAY_PASS){
+//         timerReset(blink_timer);
+//         timerReset(scroll_timer);
+//         blink = false;
+//         timerActivate(blink_timer);
+//     }
+//     display_state = DISPLAY_PASS;
+//     digit_t arr[] = {LOW_DASH, LOW_DASH, LOW_DASH, LOW_DASH};
+//     if ( position == DISPLAY_LEN){ position = DISPLAY_LEN - 1; }
+//     arr[position] = char_arr[char_idx];
+//     set_buffer(&arr[0], DISPLAY_LEN);
+//     // init timer
+// }
 
 /* 
 --------------------------------------------------------- 
@@ -166,6 +289,43 @@ void bright_down(void);
                         LOCAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
+
+// TODO:
+// BRILLO
+void refresh_display(){
+
+    switch (display_state){
+
+    case DISPLAY_SCROLL:
+        if (buffer_len <= DISPLAY_LEN){
+            set_display(&buffer[0]);
+        } else {
+            scroll_message();
+        }
+        break;
+
+    case DISPLAY_BLINK:
+        display_blink(&buffer[buffer_idx]);
+        break;
+
+    case DISPLAY_STATIC:
+        set_display(&buffer[buffer_idx]);
+        break;
+    // case DISPLAY_PASS:
+    //     display_pass();
+    //     break;
+
+    case DISPLAY_CLEAR:
+    default:
+        clear_display();
+        break;
+    }
+}
+
+void clear_display(){
+    clear_buffer();
+    buffer_idx = 0;
+}
 
 void set_display(digit_t* arr){
     uint8_t i;
@@ -188,7 +348,6 @@ void set_digit(digit_t number, uint8_t position){
         set_segment(seg_arr[idx], set);
         character = character >> 1;
     }
-
 }
 
 void set_segment(uint8_t seg, uint8_t set){
@@ -202,6 +361,85 @@ void set_buffer(digit_t *dig_arr, uint8_t arr_len){
         buffer[i] = *(dig_arr + i);
     }
     buffer_len = arr_len;
+}
+
+void auto_set_buffer_index(){
+    if (buffer_len > DISPLAY_LEN){
+        buffer_idx = buffer_len - DISPLAY_LEN;
+    } else {
+        buffer_idx = 0;
+    }
+}
+
+void scroll_buffer(){
+    digit_t* idx = &buffer[0];
+    if (scroll_idx > buffer_len){ scroll_idx = 0; }
+    if (scroll_idx + DISPLAY_LEN <= buffer_len){
+        set_display(idx + scroll_idx);
+    } else {
+        uint8_t i;
+        for (i = scroll_idx; i < buffer_len; i++){
+            set_digit(buffer[i], i - scroll_idx);
+        }
+        set_digit(CLEAR, i - scroll_idx);
+        for (i = i + 1; i < scroll_idx + DISPLAY_LEN; i++){
+            set_digit(buffer[i - buffer_len - 1], i - scroll_idx);
+        }
+    }
+}
+
+// void display_blink(){
+//     digit_t* idx;
+//     if (buffer_len < DISPLAY_LEN){
+//         idx = &buffer[0];
+//     } else {
+//         idx = &buffer[buffer_len - DISPLAY_LEN]; 
+//     }
+//     uint8_t i;
+//     for (i = 0; i < DISPLAY_LEN; i++){
+//         if ( blink && ( i == buffer_len - 1 || ( buffer_len > DISPLAY_LEN - 1 && i == DISPLAY_LEN - 1))){
+//             set_digit(CLEAR, i);
+//         } else {
+//             set_digit(*(idx + i), i);
+//         }
+//     }
+// }
+
+void display_blink(digit_t* arr){
+    uint8_t i;
+    for (i = 0; i < DISPLAY_LEN; i++){
+        if (blink && blinking_digits[i]){
+            set_digit(CLEAR, i);
+        } else {
+            set_digit(*(arr + i), i);
+        }
+    }
+}
+
+// void display_pass(){
+//     uint8_t i;
+//     for (i = 0; i < DISPLAY_LEN; i++){
+//         if ( i == buffer_len - 1 ){
+//             if (blink){
+//                 set_digit(CLEAR, i);
+//             } else {
+//                 set_digit(buffer[buffer_len - 1], i);
+//             }
+//         } else if (i < buffer_len - 1){
+//             set_digit(DASH, i);
+//         } else {
+//             set_digit(CLEAR, i);
+//         }
+//     }
+// }
+
+// callback
+void scroll_buffer(){
+    scroll_idx++;
+}
+
+void blink_digits(){
+    blink = !blink;
 }
 
 void clear_buffer(){
--- drivers/display.h
+++ drivers/display.h
@@ -3,8 +3,8 @@
   @brief    Display Driver
   @author   Olivia De Vincenti
  ******************************************************************************/
-#ifndef _ENCODER_H_
-#define _ENCODER_H_
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
 
 /*******************************************************************************
  *                      INCLUDE HEADER FILES                                    *
@@ -47,34 +47,93 @@ typedef uint8_t digit_t;
 /*******************************************************************************
  *                   FUNCTION PROTOTYPES WITH GLOBAL SCOPE                      *
  ******************************************************************************/
-//------------------------ GLOBAL ------------------------
-// INIT
-void initDisplay();                          
 
-// SETTERS
-/**
- * @brief Limpia el display, deja todos lo dígitos vacíos
- */
-void clearDisplay();
-
-
+// El display tiene 4 modos:
+   // DISPLAY_CLEAR: Limpia el display
+   // DISPLAY_SCROLL: El mensaje en buffer se desplazará por el display si tiene más de 4 dígitos
+   // DISPLAY_BLINK: Los dígitos indicados con titilarán
+   // DISPLAY_STATIC: Se mostraran los dígitos indicados, quietos
 
 /**
- * @brief Muestra en el display un mensaje de longitud variable entre 1 y 16 dígitos
- * 
- * @param message puntero al arreglo que contiene el mensaje. ESTE ARREGLO TIENE QUE CONTENER LOS CHAR IDX
- * @param message_len longitud del mensaje
+ * @brief Inicializa el driver del Display 
  */
-void showMessage(uint8_t* message, uint8_t message_len);
-
+void initDisplay();   
 
 /**
- * @brief Muestra en el display el dígito indicado en la posición indicada, rellena las posiciones anteriores con '-' (DASH)
- * 
- * @param char_idx char idx del caracter a mostrar
- * @param position posición en la que mostrar el caracter
+ * @brief Setea el modo DISPLAY_CLEAR
+ * Limpia el display
  */
-void showPasswordDigit(uint8_t char_idx, uint8_t position);
+void setClearMode();
+
+/**
+ * @brief Setea el modo DISPLAY_SCROLL
+ * El mensaje en buffer se desplazará por el display si tiene más de 4 dígitos
+ */
+void setScrollMode();
+
+/**
+ * @brief Setea el modo DISPLAY_BLINK
+ * Los dígitos indicados con setBlinkingDigits() titilarán
+ * Se puede elegir qué parte del mensaje en el buffer mostrar con setBufferIndex() o showLastDigits()
+ */
+void setBlinkMode();
+
+/**
+ * @brief Setea el modo DISPLAY_STATIC
+ * Se mostraran los dígitos indicados con setBufferIndex() o showLastDigits()
+ */
+void setStaticMode();
+                       
+// Si está en modo DISPLAY_BLINK o DISPLAY_STATIC, llamar con showLastDigits()
+/**
+ * @brief Carga en el buffer para mostrar en el display un mensaje de entre 1 y 16 dígitos
+ * @param msg Mensaje a cargar
+ * @param msg_len Longitud del mensaje, si es mayor al tamaño máximo se trunca
+ */
+void loadBuffer(digit_t *msj, uint8_t msj_len);
+
+/**
+ * @brief Configura el display para mostar los últimos 4 dígitos del buffer
+ * No aplica al modo DISPLAY_SCROLL
+ */
+void showLastDigits(bool b);
+
+/**
+ * @brief Setea la posición del buffer a partir de la cual se mostrarán los 4 dígitos
+ * @param idx Posición en el buffer
+ * No aplica al modo DISPLAY_SCROLL
+ */
+void setBufferIndex(uint8_t idx);
+
+/**
+ * @brief Configura los dígitos del display que titilarán en el modo DISPLAY_BLINK
+ * @param arr Puntero a un arreglo de 4 bools, que indican si el dígito correspondiente a la posición titila o no
+ */
+void setBlinkingDigits(bool* arr);
+
+// /**
+//  * @brief Muestra en el display un mensaje de longitud variable entre 1 y 16 dígitos
+//  * 
+//  * @param message puntero al arreglo que contiene el mensaje. ESTE ARREGLO TIENE QUE CONTENER LOS CHAR IDX
+//  * @param message_len longitud del mensaje
+//  */
+// void showMessage(uint8_t* message, uint8_t message_len);
+
+// /**
+//  * @brief Muestra en el display los dígitos que se están ingresando
+//  * 
+//  * @param input dígitos que se van ingresando
+//  * @param input_len cantidad de dígitos
+//  */
+// void showInput(uint8_t input);
+
+// /**
+//  * @brief Muestra en el display el dígito indicado en la posición indicada, rellena las posiciones anteriores con '-' (DASH)
+//  * 
+//  * @param char_idx char idx del caracter a mostrar
+//  * @param position posición en la que mostrar el caracter
+//  */
+// void showPasswordDigit(uint8_t char_idx, uint8_t position);
 
 /* 
 --------------------------------------------------------- 
@@ -91,4 +150,4 @@ void bright_down(void);
 /*******************************************************************************
  ******************************************************************************/
 
-#endif // _ENCODER_H_
+#endif // _DISPLAY_H_
