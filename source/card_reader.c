400d084b86932043b2ee8573ede84999079092d7
micho12345 <vvenier@itba.edu.ar>
2022-11-03 20:46:09 -0300

    ANDA, CAMBIE GPIO Y CARD, ver si unirlo al main

--- source/card_reader.c
+++ source/card_reader.c
@@ -9,6 +9,7 @@
  ******************************************************************************/
 #include "MK64F12.h"
 #include "gpio.h"
+#include "board.h"
 #include "card_reader.h"
 #include "board.h"
 #include <stdbool.h>
@@ -19,9 +20,9 @@
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
  //Pins from the card reader
-#define PIN_CR_DATA      DIO_16         //Card reader data entry, LS bit comes out first
-#define PIN_CR_CLOCK     DIO_17         //Card reader clock entry, data changes on positive edge
-#define PIN_CR_ENABLE    DIO_18         //Card reader enable, low while card is sliding
+#define PIN_CR_DATA      DIO_16     //Card reader data entry, LS bit comes out first
+#define PIN_CR_CLOCK     DIO_17     //Card reader clock entry, data changes on positive edge
+#define PIN_CR_ENABLE    DIO_18     //Card reader enable, low while card is sliding
 
 //Characters from the card
 #define SS ';'
@@ -40,7 +41,6 @@
 /*******************************************************************************
  * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
-static bool init;   //True after call to initCardReader()
 
 static ERROR_TYPE error_type;
 
@@ -52,19 +52,19 @@ typedef struct{
 static card_char current_char;
 
 static char Track[MAX_TRACK]; 
-static uint8_t track_index;
+static uint16_t track_index;
 
-static uint8_t stored_ID[MAX_ID];
+static uint16_t stored_ID[MAX_ID];
 
 static uint8_t data[MAX_DATA];
-static uint8_t index;
+static uint16_t index;
 
 static bool data_pin;
 
 static bool data_was_stored;
 static bool volatile enable_interrupt;
 static bool SS_arrived;
-
+static int variable = 0;
 /*******************************************************************************
  *         FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
@@ -89,32 +89,15 @@ static void add2ID (uint8_t index, uint8_t new_char);
  *******************************************************************************
  ******************************************************************************/
 void initCardReader(void){
-    if(!init){  //to avoid more than one call at a time to the Card Reader program
-        init = true;
-        error_type = NO_ERROR;
-        current_char.parity = 0b0;
-		current_char.data = 0x0;
-		current_char.loaded_bits = 0;
-        track_index = 0; 
-        index = 0;
+    gpioMode(PIN_CR_DATA, INPUT);
+    gpioMode(PIN_CR_CLOCK, INPUT);
+    gpioMode(PIN_CR_ENABLE, INPUT);
+    gpioIRQ(PIN_CR_CLOCK, GPIO_IRQ_MODE_FALLING_EDGE, irq_clk_falling_edge);
+    gpioIRQ(PIN_CR_ENABLE, GPIO_IRQ_MODE_BOTH_EDGES, irq_enable);
 
-        gpioMode(PIN_CR_DATA, INPUT);
-        gpioMode(PIN_CR_CLOCK, INPUT);
-        gpioIRQ(PIN_CR_CLOCK, GPIO_IRQ_MODE_FALLING_EDGE, irq_clk_falling_edge);
-        gpioMode(PIN_CR_ENABLE, INPUT);
-        gpioIRQ(PIN_CR_ENABLE, GPIO_IRQ_MODE_BOTH_EDGES, irq_enable);
-
-        NVIC_EnableIRQ(PORTC_IRQn);
-
-        data_was_stored = false;
-        enable_interrupt = false;
-        SS_arrived = false;
-
-    }
 }
 
 void resetReader (void){
-    init = true;
     error_type = NO_ERROR;
     data_pin = false;
     index = 0;
@@ -179,20 +162,28 @@ uint8_t* processData (void){
  *******************************************************************************
  ******************************************************************************/
 static void irq_enable () {
-    enable_interrupt = !gpioRead(PIN_CR_ENABLE);        
+	if ((gpioRead(PIN_CR_ENABLE) == 0) && (!data_was_stored)){		//Falling edge and it's not reading
+		enable_interrupt = true;									//avails to read
+	}
+    else if ((gpioRead(PIN_CR_ENABLE) == 1) && data_was_stored){    //rising edge and it's read
+        enable_interrupt = false;	
+    }
+    else {                                                          //any other case
+        enable_interrupt = false;
+    }
+
 }
 static void irq_clk_falling_edge () {	
     if (enable_interrupt && (!data_was_stored)){
-        readCard();   
+        readCard();
     }
 }
 
-static void readCard (void){
+static void readCard (void){		//CUANDO PASO LA TARJETA ENTRA MAS DE 200 VECES ACA
 
-    data_pin = !gpioRead(PIN_CR_DATA);
+    data_pin = !gpioRead(PIN_CR_DATA);				//ACA ESTA EL PROBLEMA, CUANDO LEE NUNCA ES 0
     if(index<MAX_DATA){
         pin2data(data_pin, index);          //thus, it proceeds to read incomming data
-        
     }
     else if (index == MAX_DATA){
         data_was_stored = true;
@@ -204,9 +195,9 @@ static void readCard (void){
 
 static void pin2data (bool pin, uint8_t index){
     if(!SS_arrived){
-        if (pin == 1){
+        if (pin == true){
         	SS_arrived = true;
-            write_data(pin);       
+            write_data(pin);
         }
     }
     else{       
@@ -346,6 +337,23 @@ static void add2ID (uint8_t index, uint8_t new_char){
         uint8_t converted_char = (new_char - '0');   //Converts from char to uint8_t the current number
         stored_ID[index - (MAX_ID + SPECIAL_CHAR)] = converted_char;
     }
+}
+
+void printall(void){
+    uint8_t i;
+    printf("\n ID: ");
+    for (i=0; i<8; i++){
+        printf("%d", stored_ID[i]);
+    }
+    printf("\n Track: ");
+    for (i=0; i<40; i++){
+        printf("%c", Track[i]);
+    }
+    printf("\n Error type: %d", getError());
+    /*printf("\n Data: ");
+    for (i=0; i<200; i++){
+        printf("%d", data[i]);
+    }*/
 }
 
 
--- source/gpio.c
+++ source/gpio.c
@@ -31,14 +31,14 @@ static GPIO_Type* const GPIO_PTRS[FSL_FEATURE_SOC_PORT_COUNT] = GPIO_BASE_PTRS;
 
 
 /*
-    0b0000: Interrupt/DMA request disabled
-    0b1000: Interrupt when logic 0
-    0b1001: Interrupt on rising-edge
-    0b1010: Interrupt on falling-edge
-    0b1011: Interrupt on either edge
-    0b1100: Interrupt when logic 1
+    0b1000: Interrupt/DMA request disabled
+    0b1001: Interrupt when logic 0
+    0b1010: Interrupt on rising-edge
+    0b1011: Interrupt on falling-edge
+    0b1100: Interrupt on either edge
+    0b1101: Interrupt when logic 1
 */
-static const uint32_t IRQC_MODES[] = {0b0000,0b1000,0b1001,0b1010,0b1011,0b1100};
+static const uint32_t IRQC_MODES[] = {0b1000,0b1001,0b1010,0b1011,0b1100,0b1101};
 
 
 static pinIrqFun_t CALLBACKS[ARRAY_SIZE];
@@ -60,7 +60,7 @@ static void IRQHandler(int32_t port);
  ******************************************************************************/
 void gpioMode (pin_t pin, uint8_t mode)
 {
-    
+
     // Verifico que se ingresaron los datos correctos
     if ( pin > PORTNUM2PIN(PE,31) ) return false;
 
@@ -79,34 +79,34 @@ void gpioMode (pin_t pin, uint8_t mode)
 	PORT_Type* port_ptr = PORT_PTRS[port];
 
     // Vacio el puerto:
-	port_ptr->PCR[num_pin] = 0x0;                   // Tener cuidado, se puso todo en 0!                                
-    
+	port_ptr->PCR[num_pin] = 0x0;                   // Tener cuidado, se puso todo en 0!
+
     // MUX:
     port_ptr->PCR[num_pin] |= PORT_PCR_MUX(0b001);  // 001 Alternative 1 (GPIO).
-	
+
     // DSE:
-    port_ptr->PCR[num_pin] |= PORT_PCR_DSE(0b1);    // 1 High drive strength is configured on the corresponding pin, if pin is configured as a digital output.  
+    port_ptr->PCR[num_pin] |= PORT_PCR_DSE(0b1);    // 1 High drive strength is configured on the corresponding pin, if pin is configured as a digital output.
 
     // SRE:
     // Ver final del codigo
 
     // PS & PE:
-    if ((mode==INPUT_PULLDOWN)||(mode==INPUT_PULLUP)) 
+    if ((mode==INPUT_PULLDOWN)||(mode==INPUT_PULLUP))
     {
 		port_ptr->PCR[num_pin] |= PORT_PCR_PE(0b1);     // 1 Internal pullup or pulldown resistor is enabled on the corresponding pin, if the pin is configured as a digital input.
         if(mode==INPUT_PULLDOWN)
         {
             port_ptr->PCR[num_pin] |= PORT_PCR_PS(0b0); // 0 Internal pulldown resistor is enabled on the corresponding pin, if the corresponding PE field is set.
-        } 
+        }
         else if(mode==INPUT_PULLUP)
         {
             port_ptr->PCR[num_pin] |= PORT_PCR_PS(0b1); // 1 Internal pullup resistor is enabled on the corresponding pin, if the corresponding PE field is set.
-        }    		
+        }
 	}
     //-----------------------------------------------------------------
-    
+
     //----------------------- GPIO configuration ----------------------
-    //                        55.2.6 (pag. 1803)                                                    
+    //                        55.2.6 (pag. 1803)
     GPIO_Type* gpio_ptr = GPIO_PTRS[port];
     if(mode == OUTPUT)
     {
@@ -123,16 +123,15 @@ void gpioMode (pin_t pin, uint8_t mode)
 }
 
 void gpioWrite (pin_t pin, bool value) {
-    if ( pin > PORTNUM2PIN(PE,31) ) return false;
-
-    uint32_t new_value =  (uint32_t)(1 << PIN2NUM(pin));
-    GPIO_Type* gpio_ptr = GPIO_PTRS[PIN2PORT(pin)];
-    if(value)
-    {
-        gpio_ptr->PSOR = new_value;
-    }
-    else{
-        gpio_ptr->PCOR = new_value;
+    if ( !(pin > PORTNUM2PIN(PE,31)) ){
+		uint32_t new_value =  (uint32_t)(1 << PIN2NUM(pin));
+		GPIO_Type* gpio_ptr = GPIO_PTRS[PIN2PORT(pin)];
+		if(value) {
+			gpio_ptr->PSOR = new_value;
+		}
+		else {
+			gpio_ptr->PCOR = new_value;
+		}
     }
 }
 
@@ -145,9 +144,10 @@ bool gpioRead (pin_t pin) {
 }
 
 void gpioToggle (pin_t pin) {   //no hay mucha magia, es lo mismo que el Write pero sin el if
-    if ( pin > PORTNUM2PIN(PE,31) ) return;
-    GPIO_Type* gpio_ptr = GPIO_PTRS[PIN2PORT(pin)];
-    gpio_ptr->PTOR = (uint32_t)(1 << PIN2NUM(pin));
+    if ( !(pin > PORTNUM2PIN(PE,31)) ){
+    	GPIO_Type* gpio_ptr = GPIO_PTRS[PIN2PORT(pin)];
+    	gpio_ptr->PTOR = (uint32_t)(1 << PIN2NUM(pin));
+    }
 }
 
 
@@ -178,9 +178,6 @@ bool gpioIRQ (pin_t pin, uint8_t irqMode, pinIrqFun_t irqFun) {
 }
 
 
-
-
-
 static void IRQHandler(int32_t port) {
 
 	PORT_Type* port_ptr = PORT_PTRS[port];
@@ -200,17 +197,11 @@ __ISR__ PORTD_IRQHandler(void) { IRQHandler(PD); }
 __ISR__ PORTE_IRQHandler(void) { IRQHandler(PE); }
 
 
-
-
-
-
-
-
 //----------------------------------------- Comentarios extras -------------------------------------------
 
-/* 
+/*
     SRE:
-    El SRE no se configura porque necesitamos que este en 0. Recordemos que segun Nico, el SRE es como un 
+    El SRE no se configura porque necesitamos que este en 0. Recordemos que segun Nico, el SRE es como un
     "freno de mano", si esta en Fast(0) reacciona rapido, si esta en Slow(1) tarda mucho en accionarse. Nostotros
     necesitaDAmos que sea rapido
 */
